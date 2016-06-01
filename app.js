2438cb91dd9efda164305f7d93370223d25118fb
Sean Smith <sean.smith.2009@gmail.com>
2016-06-01 13:31:01 +0000

    fixed ip address

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 
-var express = require('express');
-var accepts = require('accepts');
-var http    = require('http');
+var express   = require('express');
+var accepts   = require('accepts');
+var http      = require('http');
 var useragent = require('express-useragent');
-var port    = process.env.PORT;
-var app     = express();
+var port      = process.env.PORT;
+var app       = express();
 
 // Initialize return object:
 var reqObj = {};
 
 app.get('/', function(req, res) {
 
-    var ip   = req.connection.remoteAddress;
+    var ip  = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var lang = accepts(req).languages();
     var ops  = useragent.parse(req.headers['user-agent']);
     
     reqObj["ip address"]        = ip;
     reqObj["default langauge"]  = lang[0];
-    reqObj["brower"]   = ops.browser;
-    reqObj["version"]  = ops.version;
-    reqObj["os"]       = ops.os;
-    reqObj["platform"] = ops.platform;
+    reqObj["brower"]            = ops.browser;
+    reqObj["version"]           = ops.version;
+    reqObj["os"]                = ops.os;
+    reqObj["platform"]          = ops.platform;
     
     res.json(reqObj);
     
 });
 
 app.listen(port);
-console.log("App is listening");
+console.log("App is listening...");
