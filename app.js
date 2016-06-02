d6254ba131d5e2c9805ef08b012883eb7c8cba1c
Sean Smith <sean.smith.2009@gmail.com>
2016-06-02 22:58:42 +0000

    updated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 var express   = require('express');
 var accepts   = require('accepts');
+var fs        = require('fs');
 var http      = require('http');
 var useragent = require('express-useragent');
 var port      = process.env.PORT;
@@ -9,9 +10,17 @@ var app       = express();
 // Initialize return object:
 var reqObj = {};
 
-app.get('/', function(req, res) {
+app.get('/client/js/index.js', function(req, res) {
+    res.sendFile(__dirname + '/client/js/index.js');
+})
 
-    var ip  = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+app.get('/client/css/style.css', function(req, res) {
+    res.sendFile(__dirname + '/client/css/style.css');
+});
+
+app.get('/whoami', function(req, res) {
+
+    var ip   = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     var lang = accepts(req).languages();
     var ops  = useragent.parse(req.headers['user-agent']);
     
@@ -25,6 +34,12 @@ app.get('/', function(req, res) {
     res.json(reqObj);
     
 });
+
+app.get('*', function(res, res) {
+    
+    res.sendFile(__dirname + '/client/index.html');
+    
+})
 
 app.listen(port);
 console.log("App is listening...");
